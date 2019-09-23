fba5d78c6433ce8b8472c392ea8ecbd48391e460
Svante Schubert <svanteschubert@apache.org>
2019-09-23 11:27:18 +0200

    exercise update - including zip

--- exercises/05-add-attribute-4/run.sh
+++ exercises/05-add-attribute-4/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -javaagent:../MorganaXProc-III/MorganaXProc-III_lib/quasar-core-0.7.9.jar ../MorganaXProc-III-0.8.14-demo/MorganaXProc-III.jar pipeline.xpl -input:source=input.xml
+java -jar -javaagent:../MorganaXProc-III/MorganaXProc-III_lib/quasar-core-0.7.9.jar ../MorganaXProc-III/MorganaXProc-III.jar pipeline.xpl -input:source=input.xml
--- exercises/07-variables/run.sh
+++ exercises/07-variables/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -javaagent:../MorganaXProc-III/MorganaXProc-III_lib/quasar-core-0.7.9.jar ../MorganaXProc-III-0.8.14-demo/MorganaXProc-III.jar pipeline.xpl -input:source=input.xml -option:username=erik
+java -jar -javaagent:../MorganaXProc-III/MorganaXProc-III_lib/quasar-core-0.7.9.jar ../MorganaXProc-III/MorganaXProc-III.jar pipeline.xpl -input:source=input.xml -option:username=erik
